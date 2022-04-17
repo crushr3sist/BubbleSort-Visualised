b79b552325dddbba078e57da1fac27dc79548706
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:32:51 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-let hightA = []
+let hightA = [];
 
-function setup(){
-    let canv = createCanvas(600,600);
-    canv.center()    
+function setup() {
+  let canv = createCanvas(600, 600);
+  canv.center();
 }
 
 function draw() {
-    background(51);
-    visualize();
+  background(51);
+  visualize();
 }
 
-function visualize(){
-    for (i = 0; i < 600; i++){
-        stroke(255,30,60);
-        strokeWeight(1);
-        line( i ,600 ,i, 600-hightA[i]);
-    }
+function visualize() {
+  for (i = 0; i < 600; i++) {
+    stroke(255, 30, 60);
+    strokeWeight(1);
+    line(i, 600, i, 600 - hightA[i]);
+  }
 }
 
-function runner(){
-    bubbleSort();
-    visualize();
-
+function runner() {
+  bubbleSort();
+  visualize();
 }
-
