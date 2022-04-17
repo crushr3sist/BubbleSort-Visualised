7d893c4ad683156b5cf1cbcc017451689f24c1f5
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:33:03 +1000

    Update sorter.js

--- sorter.js
+++ sorter.js
@@ -1,34 +1,30 @@
-
-for (i = 0; i<600; i++ ){
-    var number = Math.floor(Math.random() * 600);
-    hightA.push(number);
-    var lengtH = hightA.length;
-
+for (i = 0; i < 600; i++) {
+  var number = Math.floor(Math.random() * 600);
+  hightA.push(number);
+  var lengtH = hightA.length;
 }
 
 function resett() {
+  for (l = 0; l < 600; l++) {
+    hightA.pop();
+  }
 
-    for (l = 0; l<600; l++ ){
-        hightA.pop();
-    }
-
-    for (i = 0; i<600; i++ ){
-        var number = Math.floor(Math.random() * 600);
-        hightA.push(number);
-        var lengtH = hightA.length;
-    }
+  for (i = 0; i < 600; i++) {
+    var number = Math.floor(Math.random() * 600);
+    hightA.push(number);
+    var lengtH = hightA.length;
+  }
 }
 
 function bubbleSort() {
-    for (var k = 0; k<lengtH; k++){
-        for (var j = 0; j<lengtH; j++){
-            if (hightA[j] > hightA[j+1]){
-                var temp = hightA[j];
-                hightA[j] = hightA[j+1];
-                hightA[j+1] = temp;
-                //console.log(hightA);
-            }
-        }
+  for (var k = 0; k < lengtH; k++) {
+    for (var j = 0; j < lengtH; j++) {
+      if (hightA[j] > hightA[j + 1]) {
+        var temp = hightA[j];
+        hightA[j] = hightA[j + 1];
+        hightA[j + 1] = temp;
+        //console.log(hightA);
+      }
     }
+  }
 }
-
